641b838785b9369f33dbb2c8139b49b1854c333a
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2023-01-05 15:28:33 +0800

    init

--- menu/task.js
+++ menu/task.js
@@ -6,9 +6,9 @@ menuLinkEl.forEach((link) => {
   link.addEventListener('click', (e) => {
     let menuItemEls = link.closest('.menu__item')
     let subMenu = menuItemEls.querySelector('.menu_sub')
-    if (subMenu && !subMenu.classList.contains('menu_active')) {
+    if (subMenu) {
       e.preventDefault()
-      subMenu.classList.add('menu_active')
+      subMenu.classList.toggle('menu_active')
     }
   })
 })
