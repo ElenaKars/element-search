0a672b6f6b10644a6bf36d5bb7638868b3645533
ElenaKars <87471171+ElenaKars@users.noreply.github.com>
2023-01-05 15:10:54 +0800

    done

--- menu/task.js
+++ menu/task.js
@@ -1,17 +1,14 @@
 'use strict'
 
 const menuLinkEl = Array.from(document.querySelectorAll('.menu__link'))
-const menuItem = Array.from(document.querySelectorAll('.menu__item'))
 
-menuItem.forEach((item) => {
-  const parentEl = item.closest('.menu_sub')
-
-  menuLinkEl.forEach((link) => {
-    link.addEventListener('click', (e) => {
-      if (link) {
-        e.preventDefault()
-      }
-      parentEl.classList.toggle('menu_active')
-    })
+menuLinkEl.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    let menuItemEls = link.closest('.menu__item')
+    let subMenu = menuItemEls.querySelector('.menu_sub')
+    if (subMenu && !subMenu.classList.contains('menu_active')) {
+      e.preventDefault()
+      subMenu.classList.add('menu_active')
+    }
   })
 })
